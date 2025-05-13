6b80345d0a441462777eef3a2bd5589166fe4937
admin <udalovbg7@gmail.com>
2025-05-13 21:46:19 +0400

    теперь есть админ

--- Practical.Model/Client.cs
+++ Practical.Model/Client.cs
@@ -8,5 +8,4 @@ public class Client
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public DateTime BirthDate { get; set; }
 }
